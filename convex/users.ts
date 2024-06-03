18b7ae685345c248107d2fafaa16374ee1c92cb8
Tint <98thintzawhtun@gmail.com>
2024-06-03 19:09:42 +0630

    user role base authentication

--- convex/users.ts
+++ convex/users.ts
@@ -1,25 +1,37 @@
 import { ConvexError, v } from "convex/values";
-import { internalMutation } from "./_generated/server";
+import { MutationCtx, QueryCtx, internalMutation } from "./_generated/server";
+import { UserIdentity } from "convex/server";
+
+export const getUser = async (
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string
+) => {
+  return await ctx.db
+    .query("users")
+    .withIndex("by_tokenIdentifier", (q) =>
+      q.eq("tokenIdentifier", tokenIdentifier)
+    )
+    .first();
+};
 
 export const createUser = internalMutation({
   args: { tokenIdentifier: v.string() },
   async handler(ctx, args) {
-    await ctx.db.insert("users", {
+    const users = await ctx.db.insert("users", {
       tokenIdentifier: args.tokenIdentifier,
       orgIds: [],
     });
+    if (!users) {
+      throw new ConvexError("expected user to be defined");
+    }
+    return users;
   },
 });
 
 export const addOrgIdToUser = internalMutation({
   args: { tokenIdentifier: v.string(), orgId: v.string() },
   async handler(ctx, args) {
-    const user = await ctx.db
-      .query("users")
-      .withIndex("by_tokenIdentifier", (q) =>
-        q.eq("tokenIdentifier", args.tokenIdentifier)
-      )
-      .first();
+    const user = await getUser(ctx, args.tokenIdentifier);
     if (!user) {
       throw new ConvexError("expected user to be defined");
     }
